dd93e2858c47e22e7c1b3455889fec64afdac3cd
Hisateru Tanaka <tanakahisateru@gmail.com>
2011-08-08 13:25:45 +0900

    Query condition bug fixed by Doo's way.

--- app/protected/controller/MainController.php
+++ app/protected/controller/MainController.php
@@ -13,10 +13,24 @@ class MainController extends DooController{
     }
 
 	public function detail(){
+		// sample object for query
+		Doo::loadModel('Post');
+		$sample = new Post;
+		$sample->id = $this->params['postid'];
+		
+		// single result from find only when limit=1 specified.
+		$post = Doo::db()->find($sample, array('limit'=>1));
+		
+		/* Direct query is bad practice in DooPHP because of SQL injection risk.
+		 * $post = Doo::db()->getOne('Post', array(
+		 *    'where' => 'id=' . $this->params['postid']
+		 * ));
+		 * This way is lessor than prepared statement using PDO directly.
+		 */
+		
+		//render
 		$this->render('detail', array(
-			'post'=>Doo::db()->getOne('Post', array(
-				'where'=>$this->params['postid'],
-			)),
+			'post'=>$post,
 		));
     }
 }
